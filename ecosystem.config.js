d4c528a2de46b730e74805fc70cf2e31ec7b6a51
SawyerOG <oglesbysaw@gmail.com>
2022-06-03 09:45:28 -0600

    properly named app

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,14 +1,12 @@
-module.exports = {
-	apps: [
-		{
-			name: 'receiver',
-			script: './app.js',
-			env_production: {
-				NODE_ENV: 'production',
-			},
-			env_development: {
-				NODE_ENV: 'development',
-			},
+export const apps = [
+	{
+		name: 'receiver',
+		script: './app.js',
+		env_production: {
+			NODE_ENV: 'production',
 		},
-	],
-};
+		env_development: {
+			NODE_ENV: 'development',
+		},
+	},
+];
