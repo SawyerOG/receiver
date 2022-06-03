a27a41bd3674d49ef676464b6e8d156abafa62c2
SawyerOG <oglesbysaw@gmail.com>
2022-06-03 09:52:23 -0600

    eco

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,12 +1,11 @@
-export const apps = [
-	{
-		name: 'receiver',
-		script: './app.js',
-		env_production: {
-			NODE_ENV: 'production',
+module.exports = {
+	apps: [
+		{
+			name: 'ape',
+			script: 'app.js',
+			env_production: {
+				NODE_ENV: 'production',
+			},
 		},
-		env_development: {
-			NODE_ENV: 'development',
-		},
-	},
-];
+	],
+};
